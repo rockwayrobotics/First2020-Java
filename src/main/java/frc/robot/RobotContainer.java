25b54c4583218b39cc4cde2b8b5f13c9a3543776
JoeWildfong <wildfong.joe@gmail.com>
2022-02-05 00:29:30 -0500

    Add simplified RobotContainer with inline commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,22 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.Constants.Analog;
+import frc.robot.Constants.CAN;
+import frc.robot.Constants.Controllers;
+import frc.robot.Constants.Digital;
+import frc.robot.Constants.Pneumatics;
+import frc.robot.subsystems.DrivebaseSubsystem;
+import frc.robot.subsystems.HookSubsystem;
+import frc.robot.subsystems.HopperSubsystem;
+import frc.robot.subsystems.WheelSpinnerSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -17,10 +28,26 @@ import edu.wpi.first.wpilibj2.command.Command;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private DrivebaseSubsystem m_drivebase = new DrivebaseSubsystem(
+    CAN.LEFT_MOTOR_1, CAN.LEFT_MOTOR_2,
+    CAN.RIGHT_MOTOR_1, CAN.RIGHT_MOTOR_2,
+    Digital.LEFT_ENCODER_1, Digital.LEFT_ENCODER_2,
+    Digital.RIGHT_ENCODER_1, Digital.RIGHT_ENCODER_2
+  );
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private HookSubsystem m_hook = new HookSubsystem(
+    CAN.WINCH_MOTOR, Analog.HOOK_POT
+  );
+
+  private HopperSubsystem m_hopper = new HopperSubsystem(
+    Pneumatics.HOPPER_FORWARD, Pneumatics.HOPPER_REVERSE,
+    Pneumatics.FLAP_FORWARD, Pneumatics.FLAP_REVERSE
+  );
+
+  private WheelSpinnerSubsystem m_wheelSpinner = new WheelSpinnerSubsystem(CAN.WHEEL_MOTOR);
+
+  private XboxController m_xboxController = new XboxController(Controllers.XBOX);
+  private Joystick m_flightStick = new Joystick(Controllers.FLIGHT);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -34,7 +61,42 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    m_drivebase.setDefaultCommand(
+      new RunCommand(
+        () -> m_drivebase.set(-m_xboxController.getLeftY(), m_xboxController.getLeftX(), 0),
+        m_drivebase
+      )
+    );
+
+    // halve drivebase speed while left bumper is held
+    new JoystickButton(m_xboxController, Button.kLeftBumper.value)
+      .whenPressed(() -> m_drivebase.setScale(0.5))
+      .whenReleased(() -> m_drivebase.setScale(1));
+    
+    new JoystickButton(m_xboxController, Button.kRightBumper.value)
+      .whenPressed(new InstantCommand(() -> m_wheelSpinner.spin(0.5), m_wheelSpinner))
+      .whenReleased(new InstantCommand(() -> m_wheelSpinner.spin(0), m_wheelSpinner));
+    
+
+    m_hook.setDefaultCommand(
+      new RunCommand(() -> m_hook.move(m_flightStick.getY(), 0), m_hook)
+    );
+
+    new JoystickButton(m_flightStick, 1)
+      .whenPressed(new InstantCommand(() -> m_hopper.toggle(), m_hopper));
+    
+    new JoystickButton(m_flightStick, 2)
+      .whenPressed(new InstantCommand(() -> m_hopper.flapOut(), m_hopper))
+      .whenReleased(new InstantCommand(() -> m_hopper.flapIn(), m_hopper));
+    
+    new JoystickButton(m_flightStick, 3)
+      .whenPressed(new InstantCommand(() -> m_hopper.load(), m_hopper));
+    
+    new JoystickButton(m_flightStick, 4)
+      .whenPressed(new InstantCommand(() -> m_hopper.dump(), m_hopper));
+
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -42,7 +104,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return null;
   }
 }
